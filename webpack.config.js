817dfd8fb2e13f1146d3eeb62b568241a2efe3fd
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-03-24 06:42:56 +0100

    src folder and webpackConfig created

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+modeule.export = {
+    home: './src/home.js',
+    contact: './src/contact.js',
+    completedReg: './src/completedReg.js',
+    works: './src/works.js',
+    error404: './src/error404.js',
+    error500: './src/error500.js',
+}
