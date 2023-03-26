80fbb4e838adeb8e7540fbe04d19bf90ceb1f3bd
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-03-26 18:35:28 +0200

    images & html in src and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         home: './src/home.js',
@@ -10,16 +12,52 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
-        filename: '[name].bundel.js',
-        path: __dirname + '/dist',
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    modules: {
+    module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/templates/index.html',
+            filename: 'home.html',
+            chunks: ['home']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/contact.html',
+            filename: 'contact.html',
+            chunks: ['contact']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/completedReg.html',
+            filename: 'completedReg.html',
+            chunks: ['completedReg']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/works.html',
+            filename: 'works.html',
+            chunks: ['works']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/404.html',
+            filename: 'error404.html',
+            chunks: ['error404']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/templates/500.html',
+            filename: 'error500.html',
+            chunks: ['error500']
+        }),
+    ],
 }
